2ab0b9405ff1771604f62b3ae382587f6c267fcf
SACHIN <101790234+Sachin-1412@users.noreply.github.com>
2026-02-18 13:09:06 +0530

    Delete terraform-aws directory

--- terraform-aws/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "demo_ec2" {
-  ami           = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2 (Mumbai)
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform-EC2-From-VSCode"
-  }
-}
